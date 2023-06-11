bfc034bbdd44f4ff2f7f7d18d2338cb78b04dafc
rickyfrancis <ricky.francis@my.jcu.edu.au>
2023-06-11 15:02:22 +1000

    Made program into group of functions

--- prac_02/password_stars.py
+++ prac_02/password_stars.py
@@ -1,7 +1,21 @@
-minimum_char: int = 8
-password = str(input("Enter Password: "))
-char_length = len(password)
-while char_length < minimum_char:
-    print("Invalid input. Try again.")
-    password = input("Enter Password: ")
-print('*' * char_length)
+def main():
+    minimum_char: int = 4
+    char_length = get_password(minimum_char)
+    hide_password(char_length)
+
+
+def hide_password(char_length):
+    print('*' * char_length)
+
+
+def get_password(minimum_char):
+    password = str(input("Enter Password: "))
+    char_length = len(password)
+    while char_length < minimum_char:
+        print("Invalid input. Try again.")
+        password = input("Enter Password: ")
+        char_length = len(password)
+    return char_length
+
+
+main()
