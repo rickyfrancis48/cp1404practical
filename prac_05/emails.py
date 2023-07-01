b5348ec9b648f229983bebe2c5b3cdb734bcbcfb
rickyfrancis <ricky.francis@my.jcu.edu.au>
2023-07-01 21:42:02 +1000

    Finished code file

---
+++ prac_05/emails.py
@@ -0,0 +1,39 @@
+"""
+Emails
+Estimate: 20 minutes
+Actual:   32 minutes
+"""
+
+
+def main():
+    emails = {}
+    abort = False
+    while not abort:
+        email = input("Email: ")
+        # email = "ricky.francis@my.jcu.ecu.au"
+        if email == "":
+            abort = True
+        else:
+            split_names = get_name(email)
+            joiner = " "
+            full_name = joiner.join(split_names)
+            answer = input(f"Is your name {full_name}? (Y/N) ")
+            if answer == "y" or answer == "":
+                name = full_name
+            elif answer == "n":
+                name = input("Name: ")
+            else:
+                print("Invalid response")
+            emails.update({email: name})
+
+    for person in emails:
+        print(f"{emails[person]} ({person})")
+
+
+def get_name(email):
+    email_name = email.split("@")
+    split_names = email_name[0].split(".")
+    return split_names
+
+
+main()
